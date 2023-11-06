b2b6765b3854d8ead1f57d960dfc14fe9cfad303
Meme-Paz <eameamor@gmail.com>
2023-11-06 12:16:39 -0600

    v2

--- calculadora.php
+++ calculadora.php
@@ -7,10 +7,11 @@
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
+    <h1>Bienvenidos al nuevo php</h1>
     <form class="caja" action="calculadora.php" method="get">
         <h1>CALCULADORA PHP</h1>
-        <input type="number" name="numero1" value="" placeholder="Ingrese un número">
-        <input type="number" name="numero2" value="" placeholder="Ingrese otro número">
+        <input type="number" name="numero1" value="" placeholder="Ingrese Número 1">
+        <input type="number" name="numero2" value="" placeholder="Ingrese Número 2">
 
         <input type="submit" name="" value="SUMAR">
         <output type = "number" name = "">
@@ -23,7 +24,7 @@ if (isset($_GET['numero1']) && isset($_GET['numero2'])){
     $numero1 = $_GET['numero1'];
     $numero2 = $_GET['numero2'];
     if ($numero1 === '' || $numero2 === '') {
-        echo 'Por favor, ingrese números en ambos campos.';
+        echo 'Por favor, ingrese los valores pedidos.';
     } else {
         echo $numero1 + $numero2;
     }
